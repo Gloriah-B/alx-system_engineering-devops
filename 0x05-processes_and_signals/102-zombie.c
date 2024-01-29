338cf8515a712619e2c66b1516f6bd520f049292
Gloriah-B <Gloriah-B@github.com>
2024-01-29 11:13:57 -0800

    102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Infinite loop to keep the program running
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error creating child process");
+			exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+
+	infinite_while(); /* Keep the program running */
+
+	return (0);
+}
+
