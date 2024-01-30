c9d8116b511a17ad4b669c09499a88d616935d8a
Gloriah-B <Gloriah-B@github.com>
2024-01-30 10:45:20 -0800

    1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there is exactly one command-line argument
-if ARGV.length != 1
-  puts "Usage: #{$0} <string>"
-  exit 1
-end
-
-# Extract the argument
-input_string = ARGV[0]
-
-# Define the regular expression to match the specified repetition pattern
-pattern = /hb(t{2,5})n/
-
-# Check if the input string matches the pattern
-if match = input_string.match(pattern)
-  puts match[1] # Output the matched repetition part
-else
-  puts "No match"
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
