d16783358536c69d0ad2e614ab173129505df6c0
Gloriah-B <Gloriah-B@github.com>
2024-01-30 10:34:57 -0800

    1-repetition_token_0.rb

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if there is exactly one command-line argument
+if ARGV.length != 1
+  puts "Usage: #{$0} <string>"
+  exit 1
+end
+
+# Extract the argument
+input_string = ARGV[0]
+
+# Define the regular expression to match "School" at the end of the string
+pattern = /School$/
+
+# Check if the input string matches the pattern
+if input_string =~ pattern
+  puts "#{input_string}$"
+else
+  puts "$"
+end
