bd62fc7a984dd12831d7821342c8c675ca8d64ee
Gloriah-B <Gloriah-B@github.com>
2024-01-30 10:33:02 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if there is exactly one command-line argument
-if ARGV.length != 1
-  puts "Usage: #{$0} <string>"
-  exit 1
-end
-
-# Extract the argument
-input_string = ARGV[0]
-
-# Define the regular expression
-pattern = /School/
-
-# Check if the input string matches the pattern
-if input_string =~ pattern
-  puts "#{input_string}$"
-else
-  puts "$"
-end
+puts ARGV[0].scan(/School/).join
