e5f257706b6020b31df01a850dd54a1a8e66ad62
Gloriah-B <Gloriah-B@github.com>
2024-05-07 12:40:11 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,15 +9,29 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Query Reddit API and return no of subscribers for given subreddit"""
+    """
+    Query Reddit API and return number of subscribers for given subreddit.
 
+    Args:
+    - subreddit (str): The name of the subreddit to query.
 
-url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-headers = {"User-Agent": "Mozilla/5.0"}
-response = requests.get(url, headers=headers, allow_redirects=False)
-if response.status_code == 200:
-    data = response.json()
-    subscribers = data['data']['subscribers']
-    return subscribers
-else:
-    return 0
+    Returns:
+    - int: The number of subscribers for the given subreddit. Returns 0 if
+    the subreddit is invalid.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "SubredditSubscriberCounter"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
