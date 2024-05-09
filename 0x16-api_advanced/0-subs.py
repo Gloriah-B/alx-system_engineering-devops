41372374e8c2d69cd53fa62013532dee8d57f667
Gloriah-B <Gloriah-B@github.com>
2024-05-09 12:52:51 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,32 +10,31 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries Reddit API and returns number of subscribers for given subreddit
+    Queries the Reddit API and returns no of subscribers for given subreddit
 
     Args:
         subreddit (str): The name of the subreddit to query.
 
     Returns:
-        int: The number of subscribers for the given subreddit.
-        Returns 0 if the subreddit is invalid.
+        Tuple[int, str]: A tuple containing number of subscribers for given
+        subreddit and a message indicating if the subreddit exists.
     """
-
     # Reddit API endpoint for getting subreddit information
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     # Custom User-Agent to prevent Too Many Requests error
-    headers = {
-        "User-Agent": "SubredditSubscriberCounter"
-    }
+    headers = {"User-Agent": "SubredditSubscriberCounter"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            return data['data']['subscribers'], "existing Subreddit"
+        elif response.status_code == 404:
+            return 0, "nonexisting subreddit"
         else:
             # Invalid subreddit or error occurred
-            return 0
+            return 0, "error"
     except requests.RequestException:
         # Handle request exceptions (e.g., network error)
-        return 0
+        return 0, "error"
