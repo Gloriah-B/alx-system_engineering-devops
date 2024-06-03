bc54f8ddd3c7f10f075d0e5c8f7d5291f80cdc4f
Gloriah-B <132489042+Gloriah-B@users.noreply.github.com>
2024-06-03 22:44:02 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,6 +0,0 @@
-# This manuscript enables the user holberton to login and open files without error
-
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
-}
--- 0x1B-web_stack_debugging_4/README.md
+++
@@ -1,2 +0,0 @@
-0x1B. Web stack debugging #4
-Project entails testing the web server if they are able to endure multiple requests at a go.
